6385aac971e42ad0bc4bcc346e4f9125e2f0c017
Kihong Heo <kheo@cis.upenn.edu>
2019-11-12 21:02:41 -0500

    [Env] add setups for formatting

--- setup.sh
+++ setup.sh
@@ -2,4 +2,4 @@
 
 pip3 install wllvm
 
-opam install ocamlbuild llvm.8.0.0 ctypes ctypes-foreign yojson ocamlgraph
+opam install ocamlbuild llvm.8.0.0 ctypes ctypes-foreign yojson ocamlgraph ocamlformat merlin
