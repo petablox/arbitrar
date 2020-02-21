6da3261ad18ba7a5afab6902877c7725f9170ecc
Ziyang Li <liby99@seas.upenn.edu>
2020-02-21 16:56:27 -0500

    [Script] Add script counting number of labels in the dataset

---
+++ scripts/count_labels.js
@@ -0,0 +1,26 @@
+const path_to_dir = process.cwd() + "/" + process.argv[2];
+const path_to_slices_json = path_to_dir + "/slices.json";
+const dugraphs_dir = path_to_dir + "/dugraphs/";
+const slices = require(path_to_slices_json);
+const label = process.argv[3];
+
+let count = 0;
+
+for (var i = 0; i < slices.length; i++) {
+  const slice = slices[i];
+  const callee = slice.call_edge.callee;
+  const slice_file_name = `${callee}-${i}-dugraph.json`;
+  const traces = require(dugraphs_dir + slice_file_name);
+  for (const trace of traces) {
+    if (trace["labels"]) {
+      const labels = trace.labels;
+      if (labels.indexOf(label) >= 0) {
+        count += 1;
+        process.stdout.write(`Slice Id ${i}, Count ${count}\r`);
+      }
+    }
+  }
+  delete traces;
+}
+
+console.log(`\nLabel ${label} appeared ${count} times`);
