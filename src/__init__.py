9b73f51509ecebc3eed858284b9a342d7885165d
Ziyang Li <liby99@seas.upenn.edu>
2020-03-27 15:04:03 +0000

    Setting up better argparse

--- src/__init__.py
+++ src/__init__.py
@@ -4,54 +4,36 @@ import os
 from . import collector
 from . import database
 from . import analyzer
+from . import unsupervised
+
+modules = {
+    'collect': collector,
+    'analyze': analyzer,
+    'db': database,
+    'learn': unsupervised
+}
 
 
 def init_parser():
     parser = ArgumentParser()
-    setup_parser(parser)
-    return parser
-
-
-def setup_unsup_parser(parser: ArgumentParser):
-    parser.add_argument('function', type=str, help='Function to train on')
-
-
-def setup_parser(parser: ArgumentParser):
     parser.add_argument('-d', '--db', type=str, default="data", help='Operating Database')
-
-    # Subparsers
     subparsers = parser.add_subparsers(dest="cmd")
-
-    # Collect Parser
-    collect_parser = subparsers.add_parser("collect")
-    collector.setup_parser(collect_parser)
-
-    # Analyze Parser
-    analyze_parser = subparsers.add_parser("analyze")
-    analyzer.setup_parser(analyze_parser)
-
-    # Info Parser
-    db_parser = subparsers.add_parser("db")
-    database.setup_parser(db_parser)
-
-    # Unsup Parser
-    unsup_parser = subparsers.add_parser("unsup")
-    setup_unsup_parser(unsup_parser)
+    for (key, module) in modules.items():
+        subparser = subparsers.add_parser(key)
+        module.setup_parser(subparser)
+    return parser
 
 
 def main():
     parser = init_parser()
     args = parser.parse_args()
-
     cwd = os.getcwd()
     args.cwd = cwd
-
     db = database.Database(f"{cwd}/{args.db}")
     args.db = db
 
-    if args.cmd == 'collect':
-        collector.main(args)
-    elif args.cmd == 'db':
-        database.main(args)
-    elif args.cmd == 'analyze':
-        analyzer.main(args)
+    # Execute the main function of that module
+    if args.cmd:
+        modules[args.cmd].main(args)
+    else:
+        parser.print_help()
--- src/database/src/database.py
+++ src/database/src/database.py
@@ -6,6 +6,7 @@ import json
 import subprocess
 
 from .package import *
+from .analysis import *
 from .utils import *
 
 class Database:
