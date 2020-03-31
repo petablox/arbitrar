11a18f270250d8e49dc8ed452279c32c7033b714
Anthony Canino <anthony.canino1@gmail.com>
2020-03-31 16:35:35 -0400

    Adding executables to the compile pipeline.

--- Makefile
+++ Makefile
@@ -3,7 +3,7 @@
 all: build
 
 setup:
-	pip3 install wllvm mypy flake8 autopep8 pytest scikit-learn
+	pip3 install wllvm mypy flake8 autopep8 pytest scikit-learn python-magic
 	opam install ocamlbuild ocamlformat merlin
 	opam install llvm ctypes ctypes-foreign
 	opam install ocamlgraph
--- docker/src/setup-wllvm.sh
+++ docker/src/setup-wllvm.sh
@@ -1,4 +1,8 @@
 sudo rm /usr/bin/x86_64-linux-gnu-gcc
 sudo rm /usr/bin/x86_64-linux-gnu-g++
+sudo rm /usr/bin/gcc
+sudo rm /usr/bin/g++
 sudo ln -s /usr/local/bin/wllvm /usr/bin/x86_64-linux-gnu-gcc
 sudo ln -s /usr/local/bin/wllvm++ /usr/bin/x86_64-linux-gnu-g++
+sudo ln -s /usr/local/bin/wllvm /usr/bin/gcc
+sudo ln -s /usr/local/bin/wllvm++ /usr/bin/g++
