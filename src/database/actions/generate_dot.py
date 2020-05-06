3326800bfc833f027e6149a553e11416f97fe3a2
Ziyang Li <liby99@seas.upenn.edu>
2020-05-06 05:46:19 +0000

    Better dot generation

--- src/database/actions/generate_dot.py
+++ src/database/actions/generate_dot.py
@@ -59,38 +59,62 @@ class GenerateDotAction(Executor):
   @staticmethod
   def setup_parser(parser):
     parser.add_argument('function', type=str, help="The function that the trace is about")
-    parser.add_argument('bc-file', type=str, help="The bc-file that the trace belong to")
-    parser.add_argument('slice-id', type=int, help='The slice id')
+    parser.add_argument('bc-file', type=str, help="The bc-file that the trace belong to", nargs="?")
+    parser.add_argument('slice-id', type=int, help='The slice id', nargs="?")
     parser.add_argument('trace-id', type=int, help='The trace id', nargs="?")
+
+  @staticmethod
+  def get_to_process_traces(db, var_args):
+    to_process = []
+
+    fn = var_args["function"]
+
+    if var_args["bc-file"]:
+
+      # Get all the data from command line input
+      input_bc_file = var_args["bc-file"]
+      bc_name = db.find_bc_name(input_bc_file)
+      bc = bc_name if bc_name else input_bc_file
+
+      if var_args["slice-id"]:
+        slice_id = var_args["slice-id"]
+
+        # Check if we need to generate multiple dots
+        if var_args["trace-id"]:
+          to_process = [(fn, bc, slice_id, var_args["trace-id"])]
+        else:
+          to_process = [(fn, bc, slice_id, trace_id) for trace_id in range(db.num_traces_of_slice(fn, bc, slice_id))]
+
+      else:
+
+        num_slices = db.num_slices(func_name=fn, bc=bc)
+        for slice_id in range(num_slices):
+          to_process += [(fn, bc, slice_id, trace_id) for trace_id in range(db.num_traces_of_slice(fn, bc, slice_id))]
+
+    else:
+
+      for bc in db.bc_files(full=False):
+        num_slices = db.num_slices(func_name=fn, bc=bc)
+        for slice_id in range(num_slices):
+          to_process += [(fn, bc, slice_id, trace_id) for trace_id in range(db.num_traces_of_slice(fn, bc, slice_id))]
+
+    return to_process
 
   @staticmethod
   def execute(args):
     db = args.db
-    var_args = vars(args)
 
-    # Get all the data from command line input
-    input_bc_file = var_args["bc-file"]
-    bc_name = args.db.find_bc_name(input_bc_file)
-    bc = bc_name if bc_name else input_bc_file
-
-    fn = args.function
-    slice_id = var_args["slice-id"]
-
-    # Check if we need to generate multiple dots
-    if "trace-id" in var_args and var_args["trace-id"]:
-      trace_ids = [var_args["trace-id"]]
-    else:
-      trace_ids = range(args.db.num_traces_of_slice(fn, bc, slice_id))
+    to_process = GenerateDotAction.get_to_process_traces(db, vars(args))
 
     # For each trace_id we generate dot graph
-    for trace_id in trace_ids:
+    for (fn, bc, slice_id, trace_id) in to_process:
 
       # Get the dugraph
-      dugraph = args.db.dugraph(fn, bc, slice_id, trace_id)
+      dugraph = db.dugraph(fn, bc, slice_id, trace_id)
 
       # Generate dot file
       dot = dot_of_dugraph(dugraph)
 
       # Save the file
-      save_dir = args.db.func_bc_slice_dots_dir(fn, bc, slice_id, create=True)
+      save_dir = db.func_bc_slice_dots_dir(fn, bc, slice_id, create=True)
       dot.save(filename=f"{trace_id}.dot", directory=save_dir)
