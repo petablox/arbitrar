92b9e23dbf1cd9b05cec66067e25f0ac10660d67
Ziyang Li <liby99@seas.upenn.edu>
2020-05-11 18:05:31 +0000

    Added --source option which will print the slice as source code.
    Supply --source with the root directory of the analysis. So if the
    slice has files 'tests/lock/lock_2.c', and /tests in at $HOME/files,
    you'll want to run as
    
    misapi slices mutex_lock --source $HOME/files
    
    You can adjust the amount of padding around the target line with
    --padding option (defaults to 5).

--- src/database/queries/slices.py
+++ src/database/queries/slices.py
@@ -1,3 +1,4 @@
+import os
 from os import system
 from ..meta import Executor, pp
 
@@ -7,6 +8,8 @@ class SlicesQuery(Executor):
   def setup_parser(parser):
     parser.add_argument('function', type=str)
     parser.add_argument('--location', type=str)
+    parser.add_argument('--source', type=str)
+    parser.add_argument('--padding', type=int)
 
   @staticmethod
   def execute(args):
@@ -14,6 +17,28 @@ class SlicesQuery(Executor):
     for slice_id, slice in db.function_slices(args.function):
       if not args.location or args.location in slice["call_edge"]["location"]:
         system('clear')
-        print("Slice", slice_id)
-        pp.pprint(slice)
+        if not args.source:
+            print("Slice", slice_id)
+            pp.pprint(slice)
+        else:
+            # Split in
+            toks = slice['call_edge']['location'].split(":")
+            path, func, line, col = toks[0], toks[1], int(toks[2]), toks[3]
+            print(f"Slice {slice_id} [{path}] [{(slice['call_edge']['instr']).strip()}]")
+            path = os.path.join(args.source, path)
+            padding = args.padding if args.padding else 5 
+            if not os.path.exists(path):
+                print(f"No file found at {path}") 
+            else:
+                with open(path, "r") as f:
+                    lcount = 1
+                    lmin = max(0, line-padding)
+                    lmax = line+padding
+                    for l in f.readlines():
+                        if lcount >= lmin and lcount <= lmax:
+                            pd = "==>" if lcount == line else "   "
+                            print(f"{pd} {lcount}:{l}", end="")
+                        lcount += 1 
+
+            print()
         input('Press enter to continue...')
