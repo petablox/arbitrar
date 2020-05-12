3d919580c1e08009c8ddd85b0d8b1513aba50217
Ziyang Li <liby99@seas.upenn.edu>
2020-05-12 21:19:51 +0000

    ncurses.

--- src/database/queries/alarm.py
+++ src/database/queries/alarm.py
@@ -4,6 +4,8 @@ from os import system
 import pandas
 from termcolor import cprint
 
+import curses
+
 
 class AlarmQuery(Executor):
   @staticmethod
@@ -15,6 +17,19 @@ class AlarmQuery(Executor):
 
   @staticmethod
   def execute(args):
+    stdscr = curses.initscr()
+    stdscr.clear()
+
+    columns, lines = os.get_terminal_size()
+    half_width = int(columns / 2)
+    lines = 200
+
+    left_window = curses.newwin(lines, half_width)
+    right_window = curses.newwin(lines, half_width, 0, half_width + 2)
+
+    #curses.start_color()
+    #curses.init_pair(1, curses.COLOR_RED, curses.COLOR_WHITE)
+
     db = args.db
     var_args = vars(args)
     fn = var_args["learning-dir"].split("-")[-1]
@@ -36,7 +51,6 @@ class AlarmQuery(Executor):
 
       dugraph = args.db.dugraph(fn, bc, slice_id, trace_id)
       if dugraph:
-        system('clear')
         if not args.source:
           pp.pprint(dugraph)
         else:
@@ -50,7 +64,13 @@ class AlarmQuery(Executor):
           slice = db.slice(fn, bc, slice_id)
           toks = slice['call_edge']['location'].split(":")
           path, func, line, col = toks[0], toks[1], int(toks[2]), toks[3]
-          cprint(f"Slice [{path}] {slice_id} Trace {trace_id} Alarm {i}/{nalarms}")
+
+          left_window.erase()
+          right_window.erase()
+          left_window.addstr(f"Slice [{path}] {slice_id} Trace {trace_id} Alarm {i}/{nalarms}\n")
+          right_window.addstr("Features\n")
+
+          #cprint(f"Slice [{path}] {slice_id} Trace {trace_id} Alarm {i}/{nalarms}")
           path = os.path.join(args.source, path)
           padding = args.padding if args.padding else 20
           if not os.path.exists(path):
@@ -63,12 +83,38 @@ class AlarmQuery(Executor):
               for l in f.readlines():
                 if lcount >= lmin and lcount <= lmax:
                   if lcount == line:
-                    cprint(f"==> {lcount}:{l}", "red", end="")
+                    left_window.addstr(f"==> {lcount}:{l}")
+                    #cprint(f"==> {lcount}:{l}", "red", end="")
                   else:
-                    colr = "green" if lcount in used else "white"
-                    cprint(f"    {lcount}:{l}", colr, end="")
+                    left_window.addstr(f"    {lcount}:{l}")
+                    #colr = "green" if lcount in used else "white"
+                    #cprint(f"    {lcount}:{l}", colr, end="")
                 lcount += 1
+
+          feature = args.db.feature(fn, bc, slice_id, trace_id)
+          if feature:
+            formatted = pp.pformat(feature).splitlines()
+            for l, t in enumerate(formatted):
+                if l >= lines-10:
+                    continue
+                right_window.addstr(t + "\n")
+          else:
+            right_window.addstr("No features")
       else:
         print("DUGraph does not exist")
-      print()
-      input('Press enter to continue...')
+      #print()
+      left_window.refresh()
+      right_window.refresh()
+
+      while True:
+        key = left_window.getkey()
+        if key == "n":
+            right_window.scrollok(True)
+            right_window.scroll(10)
+        elif key == "p":
+            right_window.scrollok(True)
+            right_window.scroll(-10)
+        else:
+            break
+        right_window.refresh()
+
