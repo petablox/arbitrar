4e4423a33f77828133d27fb12669e9119fc64e3a
Ziyang Li <liby99@seas.upenn.edu>
2020-05-25 16:54:56 -0400

    Temporary commit

--- src/learning/selection.py
+++ src/learning/selection.py
@@ -1,2 +1,38 @@
-def random_mask(src_ndim, dst_ndim):
-  pass
+import numpy as np
+import random
+
+class FeatureSelection:
+  def __init__(self, x):
+    self.x = x
+
+  def select(self, dst_dim
+
+class MCMCFeatureSelection:
+  """
+  Metropolis-Hastings Algorithm
+  """
+
+  def __init__(self, x):
+    super().__init__(x)
+    (_, self.dim) = np.shape(x)
+
+  def select(self, dst_dim: int, iteration=1000):
+    mask = self.random_mask(dst_dim)
+    score = self.evaluate_mask(mask)
+    for i in range(iteration):
+      next_mask = self.mutate_mask(mask, dst_dim)
+      next_score = self.evaluate_mask(mask)
+      acceptance = score / next_score
+      if acceptance > random.random():
+        mask = next_mask
+        score = next_score
+    return mask
+
+  def random_mask(self, dst_dim):
+    pass
+
+  def evaluate_mask(self, mask):
+    pass
+
+  def mutate_mask(self, mask):
+    pass
