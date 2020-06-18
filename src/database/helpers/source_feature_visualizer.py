53c581d87e828efdaac93ea7edffc30abcd64d17
Anthony Canino <anthony.canino1@gmail.com>
2020-06-18 17:58:05 +0000

    Color edits.

--- src/database/helpers/source_feature_visualizer.py
+++ src/database/helpers/source_feature_visualizer.py
@@ -17,7 +17,7 @@ class SourceFeatureVisualizer():
 
     curses.init_pair(1, curses.COLOR_YELLOW, curses.COLOR_BLACK)
     curses.init_pair(2, curses.COLOR_RED, curses.COLOR_BLACK)
-    curses.init_pair(3, curses.COLOR_BLUE, curses.COLOR_BLACK)
+    curses.init_pair(3, curses.COLOR_GREEN, curses.COLOR_BLACK)
 
     self.stdscr.clear()
 
