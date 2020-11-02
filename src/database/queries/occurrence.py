aba149ab72fd090ef7e365e07af2107b0ed0e356
Ziyang Li <liby99@seas.upenn.edu>
2020-11-01 19:54:14 -0500

    Add --lmin to occurrence check

--- src/database/queries/occurrence.py
+++ src/database/queries/occurrence.py
@@ -11,6 +11,7 @@ class OccurrenceQuery(Executor):
     parser.add_argument('-v', '--verbose', action='store_true', help="Verbose")
     parser.add_argument('-t', '--threshold', type=int, help="Occurred at least [threshold] times")
     parser.add_argument('-l', '--limit', type=int, help="Only output top [limit] results")
+    parser.add_argument('--lmin', type=int, help="Limit start")
     parser.add_argument('--bc', type=str, help="LLVM Byte Code File")
     parser.add_argument('--in-bc', nargs='+', help="BC Files")
 
@@ -83,7 +84,12 @@ class OccurrenceQuery(Executor):
           if not args.threshold or count >= args.threshold:
             counts_arr.append((func, count))
         counts_arr = sorted(counts_arr, key=lambda t: -t[1])
-        if args.limit:
-          print_counts(counts_arr[0:args.limit])
+        if args.lmin != None:
+          if args.limit != None:
+            print_counts(counts_arr[args.lmin : args.lmin + args.limit])
+          else:
+            print_counts(counts_arr[args.lmin : args.lmin + 50])
+        elif args.limit != None:
+          print_counts(counts_arr[0 : args.limit])
         else:
           print_counts(counts_arr)
